5ac056453fd819f92d125ba7a58f61cc2ab6cc53
andrealtobelli <andrerogerio03@gmail.com>
2024-09-20 21:32:52 -0300

    Adicionada validação de campos de email e senha ao perder o foco, com mensagens de erro dinâmicas.

--- login.js
+++ login.js
@@ -18,3 +18,37 @@ form.addEventListener("submit", (event) => {
     }
   }
 });
+
+email.addEventListener("blur", () => {
+    checkInputEmail();
+  });
+  
+  password.addEventListener("blur", () => {
+    checkInputPassword();
+  });
+  
+  function checkInputEmail() {
+    const emailValue = email.value;
+  
+    if (emailValue === "") {
+      errorInput(email, "O email é obrigatório.");
+    } else if (!emailValue.includes("@")) {
+      errorInput(email, "O email deve conter '@'.");
+    } else {
+      const formItem = email.parentElement;
+      formItem.className = "form-content";
+    }
+  }
+  
+  function checkInputPassword() {
+    const passwordValue = password.value;
+  
+    if (passwordValue === "") {
+      errorInput(password, "A senha é obrigatória.");
+    } else if (passwordValue.length < 6) {
+      errorInput(password, "Mínimo 6 caracteres.");
+    } else {
+      const formItem = password.parentElement;
+      formItem.className = "form-content";
+    }
+  }
