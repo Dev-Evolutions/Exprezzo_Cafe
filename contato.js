c15ff6c89b0cbcae18444c6ef1f823d7654a2bf6
enricodelguerra <ricodelguerra@gmail.com>
2024-09-22 19:35:07 -0300

    valor da mensagem e do email guardados em localstorage

--- contato.js
+++ contato.js
@@ -1,9 +1,11 @@
 document.getElementById("contato-form").addEventListener("submit", function(event) {
     const emailInput = document.getElementById("email-input").value;
+    const mensagemInput = document.getElementById("mensagem-input").value;
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-
     if (!emailRegex.test(emailInput)) {
-        event.preventDefault();
-        
+        event.preventDefault(); 
+    } else {
+        localStorage.setItem("email", emailInput);
+        localStorage.setItem("mensagem", mensagemInput);
     }
 });
