8040573b15f9ecdd4c90cad4390d53f8b7370d0d
enricodelguerra <ricodelguerra@gmail.com>
2024-09-22 19:45:42 -0300

    Span adicionado ao input email padding adicionado em inputs

--- contato.js
+++ contato.js
@@ -1,10 +1,16 @@
 document.getElementById("contato-form").addEventListener("submit", function(event) {
+    // Pegar o valor do campo de email
     const emailInput = document.getElementById("email-input").value;
     const mensagemInput = document.getElementById("mensagem-input").value;
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const emailError = document.getElementById("email-error");
+
     if (!emailRegex.test(emailInput)) {
         event.preventDefault(); 
+        
+        emailError.style.display = "inline";
     } else {
+        emailError.style.display = "none";
         localStorage.setItem("email", emailInput);
         localStorage.setItem("mensagem", mensagemInput);
     }
