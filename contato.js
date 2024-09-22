8d766377b2c90971a7247df6e02b5c85dc7b0d6c
enricodelguerra <ricodelguerra@gmail.com>
2024-09-22 20:06:58 -0300

    Span arrumado e funcional referencia do javascript alterada do head para o body

--- contato.js
+++ contato.js
@@ -3,15 +3,20 @@ document.getElementById("contato-form").addEventListener("submit", function(even
     const emailInput = document.getElementById("email-input").value;
     const mensagemInput = document.getElementById("mensagem-input").value;
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    const emailError = document.getElementById("email-error");
+    const emailError = document.getElementById("email-contato-erro");
 
     if (!emailRegex.test(emailInput)) {
         event.preventDefault(); 
-        
-        emailError.style.display = "inline";
+        emailError.style.display = "block";
+
     } else {
         emailError.style.display = "none";
+
         localStorage.setItem("email", emailInput);
         localStorage.setItem("mensagem", mensagemInput);
+        
+        console.log("email", emailInput);
+        
+
     }
 });
